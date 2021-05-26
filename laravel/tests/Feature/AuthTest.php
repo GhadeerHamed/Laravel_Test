11a5e3d421ca8298bb3d731eb7af031ed8ad6781
Ghadeer Hamed <eng.gh.a.h93@gmail.com>
2021-05-26 17:17:01 +0300

    Test case: User credentials are correct.

--- laravel/tests/Feature/AuthTest.php
+++ laravel/tests/Feature/AuthTest.php
@@ -2,13 +2,16 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
 {
     use RefreshDatabase;
+
     /**
      * A basic feature test example.
      * @test
@@ -19,5 +22,23 @@ class AuthTest extends TestCase
         $response = $this->get('/login');
 
         $response->assertStatus(200);
+    }
+
+    /**
+     * A basic feature test example.
+     * @test
+     * @return void
+     */
+    public function user_can_login_with_correct_credentials(): void
+    {
+        $user = User::query()->create([
+            'name' => 'name',
+            'email' => 'email@email.com',
+            'password' => Hash::make('password'),
+        ]);
+        $credentials = ['email' => 'email@email.com', 'password' => 'password'];
+        Auth::attempt($credentials);
+
+        $this->assertAuthenticated();
     }
 }
