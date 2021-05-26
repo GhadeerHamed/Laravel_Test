5d8f4a6571d521e4aed519941f15f89f6bb29cf0
Ghadeer Hamed <eng.gh.a.h93@gmail.com>
2021-05-26 17:12:03 +0300

    Test case: Accessing login endpoint.

---
+++ laravel/tests/Feature/AuthTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function user_can_access_login_endpoint_web(): void
+    {
+        $response = $this->get('/login');
+
+        $response->assertStatus(200);
+    }
+}
